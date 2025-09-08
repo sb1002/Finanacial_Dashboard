fe010768aad005642be419a384f9db5a1096ec16
sb1002 <sneha42b@gmail.com>
2025-09-08 17:15:00 -0400

    added bootstrap layout

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from dash import Dash, html
+from dash_bootstrap_components.themes import BOOTSTRAP
+from src.components.layout import create_layout
+
 def main() -> None:
-    pass
+    app = Dash(external_stylesheets=[BOOTSTRAP])
+    app.title = "Placeholder Title"
+    app.layout = create_layout(app)
+    app.run()
 
 if __name__ == "__main__":
     main()
